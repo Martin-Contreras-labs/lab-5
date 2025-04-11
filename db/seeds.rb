22f2f89ad49fc54d4ece198f4e732da9f58fb13c
Martin-Contreras-labs <mfcontreras3@miuandes.cl>
2025-04-11 19:01:23 -0400

    Primer commit del lab 5

---
+++ db/seeds.rb
@@ -0,0 +1,23 @@
+10.times do |i|
+    User.create!(
+      email: "usuario#{i + 1}@correo.com",
+      first_name: "Un nombre#{i + 1}",
+      last_name: "Un apellido#{i + 1}"
+    )
+  end
+
+  10.times do |i|
+    Chat.create!(
+      sender_id: ((i + 1) % 10) + 1,
+      receiver_id: ((i + 2) % 10) + 1
+    )
+  end
+
+  10.times do |i|
+    Message.create!(
+      chat_id: (i % 10) + 1,
+      user_id: ((i + 1) % 10) + 1,
+      body: "Este es uno de los varios mensajes siendo este el numero #{i + 1}"
+    )
+  end
+  
